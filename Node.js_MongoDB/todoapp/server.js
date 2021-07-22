57bee306b804875762560ebb7c584c4eaf70e962
DESKTOP-DBANJG5\woo5gi <jeonuka14@naver.com>
2021-07-22 21:48:09 +0900

    0722 로그인,회원가입,검색

--- Node.js_MongoDB/todoapp/server.js
+++ Node.js_MongoDB/todoapp/server.js
@@ -10,10 +10,13 @@ app.use('/public', express.static('public'))
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
+// 환경 변수 라이브러리
+require('dotenv').config()
+
 const MongoClient = require('mongodb').MongoClient;
 
 var db;
-MongoClient.connect('mongodb+srv://admin:1477@cluster0.y1pbj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', function (에러, client) {
+MongoClient.connect(process.env.DB_URL, function (에러, client) {
   if (에러) return console.log(에러);
 
   db = client.db('todoapp');
@@ -23,7 +26,7 @@ MongoClient.connect('mongodb+srv://admin:1477@cluster0.y1pbj.mongodb.net/myFirst
   // });
 
   //서버를 열수있음 listen(서버띄울 포트번호, 띄운후 실행할 코드)
-  app.listen('8080', function () {
+  app.listen(process.env.PORT, function () {
     console.log('listening on 8080')
   });
 })
@@ -35,26 +38,6 @@ app.get('/write', function (요청, 응답) {
   응답.render('write.ejs')
 });
 
-app.post('/add', function (요청, 응답) {
-  응답.send('전송완료');
-  console.log(요청.body.title);
-  console.log(요청.body.date);
-  
-  db.collection('counter').findOne({ name: '게시물갯수' }, function (에러, 결과) {
-    console.log(결과.totalPost);
-    var 총게시물갯수 = 결과.totalPost;
-    db.collection('post').insertOne({ _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date }, function (에러, 결과) {
-      console.log('저장완료')
-      // counter 라는 콜렉션에 있는 totalPost라는 항목 1증가시킴
-      // updateOne( {어떤데이터를 수정할지} , {수정값 } )
-      db.collection('counter').updateOne( {name : '게시물갯수' } , { $inc : { totalPost : 1 } } , function(에러, 결과){
-        if (에러) return console.log(에러);
-        console.log('수정완료')
-      })
-    });
-  });
-});
-
 app.get('/list', function (요청, 응답) {
   //모든데이터 가져오기
   db.collection('post').find().toArray(function (에러, 결과) {
@@ -63,21 +46,96 @@ app.get('/list', function (요청, 응답) {
   });
 });
 
-app.delete('/delete', function(요청, 응답){
-  요청.body._id = parseInt(요청.body._id)
-  db.collection('post').deleteOne(요청.body, function(에러, 결과){
-    console.log('삭제완료');
-    응답.status(200).send({message : '성공했습니다'});
-  })
-  응답.send('삭제완료')
+// 로그인
+// 라이브러리
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+//미들 웨어( 요청 - 응답 중간에 실행되는 코드)
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.get('/login', function(요청, 응답){
+  응답.render('login.ejs')
 });
 
-app.get('/detail/:id', function(요청, 응답){
-  db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
-    응답.render('detail.ejs', {data : 결과} )
-  })
+app.post('/login', passport.authenticate('local', {failureRedirect : '/fail'}), function(요청, 응답){
+  응답.redirect('/list')
 });
 
+// app.get('/fail', function(요청, 응답){
+//   응답.send('로그인실패');
+// });
+
+passport.use(new LocalStrategy({
+  usernameField: 'id', // ( 사용자가 제출한 아이디가 어디 적혔는지)
+  passwordField: 'pw', // ( 사용자가 제출한 비번이 어디 적혔는지)
+  session: true, // ( 세션을 만들건지)
+  passReqToCallback: false,//  ( 아이디/비번말고 다른 정보검사가 필요한지)
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+    
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디입니다.' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비밀번호 다릅니다.' })
+    }
+  })
+}));
+
+// 세션을 저장시키는 코드 (로그인 성공시 발동)
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+});
+
+passport.deserializeUser(function (아이디, done) {
+  db.collection('login').findOne({ id: 아이디 }, function (에러, 결과) {
+    done(null, 결과)
+  })
+}); 
+
+//회원가입
+app.post('/register', function (요청, 응답) {
+  db.collection('login').insertOne({ id: 요청.body.id, pw : 요청.body.pw }, function (에러, 결과) {
+    응답.redirect('/login')
+  });
+});
+
+// 마이페이지
+app.get('/mypage', loginOk, function (요청, 응답) {
+  console.log(요청.user);
+  응답.render('mypage.ejs', {사용자 : 요청.user})
+})
+
+function loginOk(요청, 응답, next) {
+  if (요청.user) {
+    next()
+  } else {
+    응답.send('로그인 하셔야 합니다?')
+  }
+}
+
+app.post('/add', loginOk,function (요청, 응답) {
+  console.log(요청.user._id)
+  //응답.send('전송완료');
+  db.collection('counter').findOne({ name: '게시물갯수' }, function (에러, 결과) {
+    var 총게시물갯수 = 결과.totalPost;
+    var post = { _id: 총게시물갯수 + 1, 작성자: 요청.user._id , 제목: 요청.body.title, 날짜: 요청.body.date }
+    db.collection('post').insertOne( post , function (에러, 결과) {
+      db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
+        if (에러) { return console.log(에러) }
+      })
+    });
+    응답.redirect('/list')
+  });
+});
+
+//수정
 app.get('/edit/:id', function (요청, 응답) {
   db.collection('post').findOne({ _id: parseInt(요청.params.id) }, function (에러, 결과) {
     응답.render('edit.ejs', {post : 결과} );
@@ -89,5 +147,38 @@ app.put('/edit', function(요청, 응답){
   db.collection('post').updateOne( {_id : parseInt(요청.body.id) }, {$set : { 제목 : 요청.body.title , 날짜 : 요청.body.date }}, function(에러,결과){
     console.log('수정완료');
     응답.redirect('/list');
+  });
+});
+
+// 삭제
+app.delete('/delete', function (요청, 응답) {
+  요청.body._id = parseInt(요청.body._id);
+  //요청.body에 담겨온 게시물번호를 가진 글을 db에서 찾아서 삭제해주세요
+  db.collection('post').deleteOne({_id : 요청.body._id, 작성자 : 요청.user._id }, function (에러, 결과) {
+    console.log('삭제완료');
+    console.log('에러',에러)
+    응답.status(200).send({ message: '성공했습니다' });
+  })
+});
+
+// 서버에서 query string 확인
+app.get('/search', (요청, 응답)=>{
+  var 검색조건 = [
+    {
+      $search: {
+        index: 'titleSearch',
+        text: {
+          query: 요청.query.value,
+          path: '제목'  // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
+        }
+      }
+    },
+    { $sort : { _id : 1 } },
+    { $limit : 10 }
+  ] 
+  console.log(요청.query);
+  db.collection('post').aggregate(검색조건).toArray((에러, 결과)=>{
+    console.log(결과)
+    응답.render('search.ejs', {posts : 결과})
   });
 });

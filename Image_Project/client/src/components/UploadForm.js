da6468d1ddad30e4b95f83291b326d3870093d94
DESKTOP-DBANJG5\woo5gi <jeonuka14@naver.com>
2021-08-04 19:17:04 +0900

    0804 이미지관리풀스택(데이터베이스 연동해서 이미지 관리하기)

--- Image_Project/client/src/components/UploadForm.js
+++ Image_Project/client/src/components/UploadForm.js
@@ -29,7 +29,7 @@ const UploadForm = () => {
     formData.append("image", file);
 
     try {
-      const res = await axios.post("/upload", formData, {
+      const res = await axios.post("/images", formData, {
         headers: { "Content-Type": "multipart/form-data" },
         onUploadProgress: (e) => {
           setPercent(Math.round((100* e.loaded) / e.total));
--- Image_Project/server/server.js
+++ Image_Project/server/server.js
@@ -1,35 +1,64 @@
+require("dotenv").config();
 const express = require("express");
 const multer = require('multer');
 const { v4: uuid } = require('uuid');
 const mime = require("mime-types");
+const mongoose = require("mongoose");
+const Image = require("./models/Image");
+
 
 // 이미지 업로드
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb (null, "./uploads"),
-  filename: (req, file, cb) => 
-  cb(null,`${uuid()}.${mime.extension(file.mimetype)}`),
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) =>
+    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+});
+
+// 이미지 필터
+const upload = multer({
+  storage, fileFilter: (req, file, cb) => {
+    if (["image/png", "image/jpeg"].includes(file.mimetype)) cb(null, true);
+    else cb(new Error("invalid file type"), false);
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  }
 });
 
 const app = express();
 const PORT = 5000;
 
-// 이미지 필터
-const upload = multer({
-  storage, fileFilter: (req, file, cb) => {
-    if(["image/png", "image/jpeg"].includes(file.mimetype)) cb(null,true);
-    else cb(new Error("invalid file type"), false);
-  },
-  limits:{
-    fileSize: 1024 * 1024 * 10,
-  }
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("mongodb connected");
+
+    // 이미지 파일들 외부로 노출시켜주기
+    app.use("/uploads", express.static("uploads"));
+   
+    // 이미지 업로드
+    app.post("/images", upload.single("image"), async (req, res) => {
+     const image = await new Image({ 
+        key: req.file.filename, 
+        originalFileName: req.file.originalname 
+      }).save();
+      res.json(image);
+      // console.log(req.file);
+    });
+   
+    //사진정보 가져오기
+    app.get("/images", async(req,res) => {
+      const images = await Image.find();
+      res.json(images);
+    });
+   
+    app.listen(PORT, () =>
+      console.log("Express server listening on PORT " + PORT)
+    );
+  }).catch((err) => console.log(err));
 
 
-// 이미지 파일들 외부로 노출시켜주기
-app.use("/uploads",express.static("uploads"));
-
-app.post("/upload", upload.single("image"), (req, res) => res.json(req.file));
-  // return res.status(500).json({error: "server failure"});
-  // res.json(req.file);
-  
-app.listen(PORT, () => console.log("Express server listening on PORT " + PORT))

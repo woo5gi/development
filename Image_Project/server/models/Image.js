e85e87a6b454d333dae6a40b0838003219659ed4
DESKTOP-DBANJG5\woo5gi <jeonuka14@naver.com>
2021-08-06 14:46:12 +0900

    0805 이미지관리풀스택(Authorization & 사진첩 서비스-server)

--- Image_Project/server/models/Image.js
+++ Image_Project/server/models/Image.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 const ImageSchema = new mongoose.Schema(
   {
+    user: {
+      _id: { type: mongoose.Types.ObjectId, required: true, index: true },
+      name: { type: String, required: true },
+      username: { type: String, required: true },
+    },
+    likes: [{ type: mongoose.Types.ObjectId }],
+    public: { type: Boolean, required: true, default: false },
     key: { type: String, required: true },
     originalFileName: { type: String, required: true },
   },

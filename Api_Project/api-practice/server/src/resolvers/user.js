1bbe43e2379abc199cf2a72c41f95e890e5335cf
jeonuka14 <jeonuka14@naver.com>
2021-09-02 11:25:35 +0900

    삭제

--- Api_Project/api-practice/server/src/resolvers/user.js
+++
@@ -1,8 +0,0 @@
-const userResolver = {
-  Query: {
-    users: (parent, args, { db }) => Object.values(db.users),
-    user: (parent, { id }, { db }) => db.users[id],
-  },
-}
-
-export default userResolver

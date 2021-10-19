85a640b09508752af06e5574bacb3fd80f354483
jeonuka14 <jeonuka14@naver.com>
2021-10-19 19:12:44 +0900

    삭제

--- Image_Project/server/routes/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  ...require("./imageRouter"),
-  ...require("./commentRouter"),
-  ...require("./userRouter"),
-  ...require("./blogRouter"),
-};
--- MongoDB/src/models/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  ...require("./Blog"),
-  ...require("./Comment"),
-  ...require("./User"),
-};
